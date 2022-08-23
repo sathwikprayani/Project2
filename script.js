03bfb09c71189b775bb2555cefbe9372940ef870
FoxBitbucket <shift_mgrs@dazsi.com>
2022-08-23 14:55:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var text = document.getElementById("c1");
+var strText = text.textContent;
+var splitText = strText.split("");
+text.textContent = "";
+for(var i=0; i< splitText.length; i++){
+  text.innerHTML += "<span>"+ splitText[i] + "</span>";
+}
+
+var char = 0;
+var timer = setInterval(onTick, 600);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++
+  if(char === splitText.length){
+    complete();
+    return;
+   }
+  }
+
+  function complete(){
+    clearTimeout(timer);
+    timer = null;
+  }
